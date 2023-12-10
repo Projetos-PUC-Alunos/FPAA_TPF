e5e886624fcf6e488fa0a0af51c9b3a3fd77a0c2
coutinholaura <110981345+coutinholaura@users.noreply.github.com>
2023-12-10 12:23:47 -0300

    Update Divisao_Conquista.py

--- divisaoconquista/Divisao_Conquista.py
+++ divisaoconquista/Divisao_Conquista.py
@@ -49,25 +49,3 @@ def divide_rotas(rotas, num_caminhoes):
             melhor_distribuicao = distribuicao_atual
 
     return melhor_distribuicao or [[] for _ in range(num_caminhoes)]
-
-# def divide_rotas(rotas, num_caminhoes):
-#     if len(rotas) == 0:
-#         return [[] for _ in range(num_caminhoes)]
-
-#     if num_caminhoes == 1:
-#         return [rotas]
-
-#     melhor_distribuicao = None
-#     menor_diferenca = float('inf')
-
-#     for i in range(1, len(rotas)):
-#         distribuicao_atual = [rotas[:i]] + divide_rotas(rotas[i:], num_caminhoes - 1) or [[] for _ in range(num_caminhoes - 1)]
-
-#         quilometragens = [sum(caminhao) for caminhao in distribuicao_atual]
-#         diferenca = diferenca_entre_caminhoes(quilometragens)
-
-#         if diferenca < menor_diferenca:
-#             menor_diferenca = diferenca
-#             melhor_distribuicao = distribuicao_atual
-
-#     return melhor_distribuicao or [[] for _ in range(num_caminhoes)]
